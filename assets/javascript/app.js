80d031d95ad768b2a454380546d34d23bde504c2
Miles Kenner <mdk0109@yahoo.com>
2019-01-03 11:18:41 -0600

    Some javascript functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,35 @@
 // Timer information
 
+
+$(document).ready(function() {
+
+
+
+
+// questions
+var q1 = "1.) Jupiter is the largest planet in the solar system."
+
+
+
+$("#startbtn").on("click", function() {
+$("#q1").text(q1);
+console.log(q1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
