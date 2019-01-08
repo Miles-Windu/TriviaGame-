8036b8f30726a34b2d4683d719697d282c1e7d60
Miles Kenner <mdk0109@yahoo.com>
2019-01-08 12:47:29 -0600

    Start and Done buttons added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,4 @@
-// Timer information
-
-
 $(document).ready(function() {
-
 
 // questions
 var q1 = "1.) Jupiter is the largest planet in the solar system.";
@@ -13,27 +9,70 @@ var q5 = "5.) Water was recently discovered on Mars suggesting signs of life.";
 var q6 = "6.) There are more stars in the galaxy than there are grains of sand on the Earth's beaches.";
 var q7 = "7.) Neptune was named after the Greek God of thunder."; 
 var q8 = "8.) You can fit almost 1,000,000 Earths on the Sun's surface.";  
-var choices = "<form><input type= 'radio' value='true' name='true/false' checked> True <input type='radio' value='false' name='true/false'>False </form>";
+
+// True or false values added to the DOM after each question 
+var choices = "<form><input type= 'radio' value='true' name='true/false'> True <input type='radio' value='false' name='true/false'>False </form>";
+
+var correct = 0;
+var wrong = 0; 
+var unanswered = 0; 
 
 // When the start button is pressed then the questions populate and the start button disapprears
 $("#startbtn").on("click", function() {
     $("#startbtn").remove();
-$("#q1").text(q1);
-$("#q1").append(choices)
-$("#q2").text(q2);
-$("#q2").append(choices)
-$("#q3").text(q3);
-$("#q3").append(choices)
-$("#q4").text(q4);
-$("#q4").append(choices)
-$("#q5").text(q5);
-$("#q5").append(choices)
-$("#q6").text(q6);
-$("#q6").append(choices)
-$("#q7").text(q7);
-$("#q7").append(choices)
-$("#q8").text(q8);
-$("#q8").append(choices)
+
+$("#q1").text(q1); // call question 
+$("#q1").append(choices) //display true or false buttons
+
+$("#q2").text(q2); // call question
+$("#q2").append(choices) //display true or false buttons
+ 
+$("#q3").text(q3); // call question
+$("#q3").append(choices) //display true or false buttons
+
+$("#q4").text(q4); // call question
+$("#q4").append(choices) //display true or false buttons
+
+$("#q5").text(q5); // call question
+$("#q5").append(choices) //display true or false buttons
+
+$("#q6").text(q6); // call question
+$("#q6").append(choices) //display true or false buttons
+
+$("#q7").text(q7); // call question
+$("#q7").append(choices) //display true or false buttons
+
+$("#q8").text(q8); // call question
+$("#q8").append(choices) //display true or false buttons
+
+// Appending the finish button into the DOM 
+$("#done").append("<div class='col-4'></div>", "<button id='finishbtn' class='col-4 btn btn-primary btn-lg'> Done! </button>")
+
+
+
+
+
+
+// upon completion tasks 
+$("#finishbtn").on("click", function() {
+$("#finishbtn").remove();
+$("#q1").remove();
+$("#q2").remove();
+$("#q3").remove();
+$("#q4").remove();
+$("#q5").remove();
+$("#q6").remove();
+$("#q7").remove();
+$("#q8").remove();
+
+$("#timer").append("<div class='text-center p-3 mb-3 text-dark mt-2' style='font-size: 48px;'>All Done!</div>");
+$("#timer").append("<div class='text-center p-3 mb-3 text-dark mt-2' style='font-size: 30px;'>Correct Answers: </div>", correct);
+$("#timer").append("<div class='text-center p-3 mb-3 text-dark mt-2' style='font-size: 30px;'>Incorrect Answers: </div>", wrong);
+$("#timer").append("<div class='text-center p-3 mb-3 text-dark mt-2' style='font-size: 30px;'>Unanswered: </div>", unanswered);
+
+
+}); 
+
 
 
 
