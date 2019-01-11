6d93383e2fcb3fed684e60f85134118b9cee7d69
Miles Kenner <mdk0109@yahoo.com>
2019-01-11 08:11:55 -0600

    Incomplete Javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,48 +10,53 @@ var q6 = "6.) There are more stars in the galaxy than there are grains of sand o
 var q7 = "7.) Neptune was named after the Greek God of thunder."; 
 var q8 = "8.) You can fit almost 1,000,000 Earths on the Sun's surface.";  
 
-// True or false values added to the DOM after each question 
-var choices = "<form><input type= 'radio' value='true' name='true/false'> True <input type='radio' value='false' name='true/false'>False </form>";
+var choices = "<form><input name='true/false' type='radio' value='true'> True <input type='radio' name='true/false' value='false'>False </form>";
 
 var correct = 0;
 var wrong = 0; 
-var unanswered = 0; 
+var unanswered = 8; 
+
+var correctArray = [];
+var incorrectArray = ["true", "true", "false", "false", "false", "true", "false", "true"]; 
 
 // When the start button is pressed then the questions populate and the start button disapprears
 $("#startbtn").on("click", function() {
     $("#startbtn").remove();
 
 $("#q1").text(q1); // call question 
-$("#q1").append(choices) //display true or false buttons
+$("#q1").append(choices); //display true or false buttons
+
 
 $("#q2").text(q2); // call question
 $("#q2").append(choices) //display true or false buttons
  
+
 $("#q3").text(q3); // call question
 $("#q3").append(choices) //display true or false buttons
+
 
 $("#q4").text(q4); // call question
 $("#q4").append(choices) //display true or false buttons
 
+
 $("#q5").text(q5); // call question
 $("#q5").append(choices) //display true or false buttons
+
 
 $("#q6").text(q6); // call question
 $("#q6").append(choices) //display true or false buttons
 
+
 $("#q7").text(q7); // call question
 $("#q7").append(choices) //display true or false buttons
+
 
 $("#q8").text(q8); // call question
 $("#q8").append(choices) //display true or false buttons
 
+
 // Appending the finish button into the DOM 
 $("#done").append("<div class='col-4'></div>", "<button id='finishbtn' class='col-4 btn btn-primary btn-lg'> Done! </button>")
-
-
-
-
-
 
 // upon completion tasks 
 $("#finishbtn").on("click", function() {
@@ -72,9 +77,6 @@ $("#timer").append("<div class='text-center p-3 mb-3 text-dark mt-2' style='font
 
 
 }); 
-
-
-
 
 });
 
